41fca18f11455c8f8509759a9877b07a8fdcfe45
joyrapee <Refrisop@protonmail.com>
2025-03-06 13:36:36 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print("Hello, World!")
+def say_hello(name):
+    """Function that returns a greeting message."""
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")  # Ask for user input
+    print(say_hello(user_name))  # Print the greeting message
